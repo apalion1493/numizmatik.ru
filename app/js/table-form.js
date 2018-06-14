33c9cf301def007b542519fc88700fc48dfd8ec0
Alexandr Petrovich <apalion97@gmail.com>
2018-06-14 16:04:09 +0300

    v1.3

---
+++ app/js/table-form.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    (function quantityProducts() {
+        var $quantityArrowMinus = $(".quantity-arrow-minus");
+        var $quantityArrowPlus = $(".quantity-arrow-plus");
+        var $quantityNum = $(".quantity-num");
+
+        $quantityArrowMinus.click(quantityMinus);
+        $quantityArrowPlus.click(quantityPlus);
+
+        function quantityMinus() {
+            if ($quantityNum.val() > 1) {
+                $quantityNum.val(+$quantityNum.val() - 1);
+            }
+        }
+
+        function quantityPlus() {
+            $quantityNum.val(+$quantityNum.val() + 1);
+        }
+    })();
+
+});

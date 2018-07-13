b738fae6be63f5979ebeda5040eafb78c90efc34
Alexandr Petrovich <apalion97@gmail.com>
2018-07-13 17:10:40 +0300

    v4.7

--- app/js/recommendations.js
+++ app/js/recommendations.js
@@ -34,4 +34,40 @@ jQuery(document).ready(function($) {
             // instead of a settings object
         ]
     });
+
+    $('.recommendations__content[data-slick-slider-default-3]').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });

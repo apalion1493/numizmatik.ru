aa9e4f47ac03c979c3518aa2cd7091a7dfc32bd5
“Grosu <grsvtl@gmail.com>
2018-08-03 15:04:59 +0300

    Cart Plus minus

---
+++ app/js/cart.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+
+    // Calc
+    $('.cart-table').change(function(){cartTableCalc();});
+    $('button[data-minus]').click(function(){elPlusMin($(this).data('minus'));});
+    $('button[data-plus]').click(function(){elPlusMin($(this).data('plus'),true);});
+    $('.cart-table__last-cell-link').click(function(){$(this).parent('tr').remove();});
+    
+    function cartTableCalc() {
+		var total = 0;
+		
+		$('input[data-id]').each(function() {
+			var idElement = '#'+$(this).data('id'),
+				price = parseInt($(idElement+'_price').text()),
+				sum = parseInt($(this).val());
+			
+			$(idElement+'_sum').text(price * sum);
+			total += price * sum;
+		});
+		$('.cart-table__totals-price b').text(total);
+	}
+    
+    
+    function elPlusMin(el,plus=false){
+		var that=$('input[data-id="'+el+'"]'),count=that.val();
+		if (plus==true) count++; else if (count>0) count--;
+		that.val(count);
+        cartTableCalc();
+	}
+    
+});

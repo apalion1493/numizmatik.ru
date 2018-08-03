1acc0aedb867b36e44383f4f29f10e479b006870
“Grosu <grsvtl@gmail.com>
2018-08-03 11:51:58 +0300

    Search Filter

--- app/js/script.js
+++ app/js/script.js
@@ -24,4 +24,77 @@ jQuery(document).ready(function($) {
     });
     
     
+    // Search Filter
+    countryFilter = $('.country-filter__radio-group input');
+    $('.country-filter-search__form').keyup(function(){
+        
+        var query = $('.country-filter-search__form').val().toLowerCase(),
+            category = $('.country-filter__radio-group input:checked').val(),
+            field = '.country-filter__main';
+        
+        switch(category) {
+            case 'alphabet':
+                field='.country-filter__main';
+                break;
+            case 'krause':
+                field='.continents-filter__main-krause';
+                break;
+            case 'continents':
+            default:
+                return false;
+        }
+        
+        var r=$(field).find(' .form-check'),th=this;
+
+        r.each(function(i,v){
+            console.log($(v).text().toLowerCase().indexOf($(th).val().toLowerCase()));
+            if($(v).text().toLowerCase().indexOf($(th).val().toLowerCase())==-1)
+                r.eq(i).hide(); 
+            else r.eq(i).show();
+        })
+        /*
+        if(query.length>2){
+            $(field+' .checkbox-block').each(function(i, el) {	 	
+	          if ($(el).find('a').text().toLowerCase().indexOf(query) != -1) {
+	              $(el).show();
+	          }else{
+	              $(el).hide();
+	          }         
+            });	     
+        } else {
+            $('#fb-groups .checkbox-block').each(function(i, el) {			    	
+                $(el).show();
+            });
+        }  */
+        
+        /*var scroll_element = $(".lab_group_scroll");			
+        var element = $(scroll_element).jScrollPane();         	   	
+        var api = element.data('jsp');
+        if(api) api.reinitialise();   */
+    });
+
+
 });
+
+
+//SearchTable
+$('#tbl-search').keyup(function(){
+    var r=$('.table tbody').find('tr'),th=this;
+    r.each(function(i,v){
+        if($(v).text().toLowerCase().indexOf($(th).val().toLowerCase())==-1)
+            r.eq(i).hide(); 
+        else r.eq(i).show()
+    })
+});
+
+//SearchSelect
+function SearchSelect(a){
+    $(a+'_s').keyup(function(){
+        var p=$(a),r=p.find('option'),th=this;
+        r.each(function(i,v){
+            if($(v).text().toLowerCase().indexOf($(th).val().toLowerCase())==-1)
+                r.eq(i).hide(); 
+            else{r.eq(i).show();p.val(r.eq(i).val());}
+        });
+    });
+}

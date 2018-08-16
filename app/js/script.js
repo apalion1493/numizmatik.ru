a52a5d6ad0abb4f2280e44ac6098af19c1433717
Alexandr Petrovich <apalion97@gmail.com>
2018-08-16 11:21:51 +0300

    script2

--- app/js/script.js
+++ app/js/script.js
@@ -1,36 +1,36 @@
-jQuery ( документ ). ready ( function ( $ ) {
+jQuery(document).ready(function($) {
 
-    // Прокрутить вверх
-    var _header_h = $ ( ' .page-header ' ). height ();
+    // Scroll To Top
+    var _header_h=$('.page-header').height();
 
-    function  backTop_Scroll () {
-        if ( jQuery ( this ). scrollTop () > _header_h) {
-            $ ( ' .back-to-top ' ). show ();
+    function backTop_Scroll(){
+        if (jQuery(this).scrollTop()>_header_h) {
+            $('.back-to-top').show();
         } else {
-            $ ( ' .back-to-top ' ). hide ();
+            $('.back-to-top').hide();
         }
     }
 
-    backTop_Scroll ();
+    backTop_Scroll();
 
-    jQuery ( окно ). scroll ( function () {
-        backTop_Scroll ();
+    jQuery(window).scroll(function(){
+        backTop_Scroll();
     });
 
-    $ ( ' .back-to-top ' ). click ( function () {
-        $ ( ' html, body ' ). анимации ({scrollTop : 0 }, 999 ); return  false ;
-    });
-
-
-    // Добавить скидку
-    $ ( ' .btn-discount-code-ext ' ). click ( function () {
-        $ ( ' .discount-code__forms-block ' ). last (). клон (). appendTo ( $ ( ' .discount-code__forms ' ));
+    $('.back-to-top').click(function(){
+        $('html,body').animate({scrollTop:0},999);return false;
     });
 
 
+    // Add discount
+    $('.btn-discount-code-ext').click(function() {
+        $('.discount-code__forms-block').last().clone().appendTo($('.discount-code__forms'));
+    });
 
 
-    // Фильтр поиска
+
+
+    // Search Filter
 
     countryFilter = $('.country-filter__radio-group input');
 
